e02de4e9e858498ab8e6397d9fe57b079ece9928
mothibimalebana <mothibi.mm12@gmail.com>
2023-05-19 01:28:01 +0200

    puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - A program that prints a string using puts.
+ *
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like a multilingual langauge\n");
+	return (0);
+}
