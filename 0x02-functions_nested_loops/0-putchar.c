1bad36cad1c647729eef5c50a2651b8cf8152b77
mothibimalebana <mothibi.mm12@gmail.com>
2023-05-23 23:12:26 +0200

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main: executes the program
+ * @void: main function has no argurments
+ *
+ */
+
+int main(void)
+{
+        printf("_putchar\n");
+        
+        return (0);
+}
