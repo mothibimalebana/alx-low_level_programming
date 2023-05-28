562eef14dce2412f1e0c5c082d1ad2d6c876a889
mothibimalebana <mothibi.mm12@gmail.com>
2023-05-29 00:04:42 +0200

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * main - tests the positive/negative function.
+ * @void: main function takes no parameters.
+ * Return: main function takes no return value.
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	
+	return (0);
+}
