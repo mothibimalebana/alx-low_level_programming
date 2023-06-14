54601f36170a739b7e86ac1542e20cfc398a4089
mothibimalebana <mothibi.mm12@gmail.com>
2023-06-14 09:11:36 +0200

    0-positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,42 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - startind point for program execution.
- * 
- * @void: main function does not take any parameters.
- * 
-* @void: main function does not take any parameters.
- * 
- * Return: main fucntion does not have any parameters.
- * Return: main fucntion does not have any parameters.
- * 
-* Return: main fucntion does not have any parameters.
- * 
-* Description: program generates random numbers and determines if even or odd.
-*/
-
+ * main - starting point for program execution.
+ *
+ * @void: main function has no parameters.
+ * Return: main function has no return values.
+ * Description: program must determine if n is positive or negatibe.
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	/* your code goes there */
 
-/* your code goes there */
-
-    if(n >= 0)
-    {
-        printf("%d is positive\n",n);
-    }
-    else if(n == 0)
-    {
-        printf("%d is zero\n");
-    }
-    else
-    {
-        printf("%d is negative\n",n);
-    }
+	if (n >= 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 	return (0);
 }
