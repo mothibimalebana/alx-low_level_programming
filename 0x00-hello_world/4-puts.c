16f03255a49b16bdc0b9484f493f93997aae6391
mothibimalebana <mothibi.mm12@gmail.com>
2023-12-14 14:55:04 +0200

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[100] = "Programming is like building a multilingual puzzle\n";
+
+	puts(c);
+
+	return (0);
+}
