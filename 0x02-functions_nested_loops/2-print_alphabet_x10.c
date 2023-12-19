447fb9e5866fd0a2f0da32de46fb7c1df4788385
mothibimalebana <mothibi.mm12@gmail.com>
2023-12-19 21:31:37 +0200

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Function that prints the alphabet 10x in lowercase.
+ *
+ * Return: void.
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+
+	counter = 0;
+
+	while (counter < 10)
+	{
+		char i = 'a';
+
+		for (; i < 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+		counter++;
+	}
+}
