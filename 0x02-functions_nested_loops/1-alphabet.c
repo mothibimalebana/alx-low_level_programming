3eb39ffc16b45dfbe17e1cacf96bf9aae62bb241
mothibimalebana <mothibi.mm12@gmail.com>
2023-12-19 21:08:49 +0200

    sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints letter of alphabet in order.
+ *
+ * Return: Returns letter of alphabet.
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	for (; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - Prints letter of alphabet in order.
+ *
+ * Return: Returns letter of alphabet.
+ */
+void print_alphabet(void)
+{
+	for(char i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	return (0);
+}
