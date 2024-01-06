61e00a9a445cda3786a061dab808afabb1861c25
mothibimalebana <mothibi.mm12@gmail.com>
2024-01-07 01:30:06 +0200

    I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts - Function that prints a string.
+ * @str: String to be printed.
+ *
+ * Return: void. (nothing)
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar('\n');
+}
