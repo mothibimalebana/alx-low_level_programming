660deec52d80c6192cef17f9a7cebf77190aee33
mothibimalebana <mothibi.mm12@gmail.com>
2024-01-13 18:57:21 +0200

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -19,10 +19,10 @@ char *_strncat(char *dest, char *src, int n)
 
 	for (j = 0; src[j] != '\0' && j < n; j++)
 	{
-		dest[i+j] = src[j];
+		dest[i + j] = src[j];
 	}
 
-	dest[j] = '\0';
+	dest[i + j] = '\0';
 
 	return (dest);
 }
