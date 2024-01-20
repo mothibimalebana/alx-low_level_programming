1c580f36c435ac6c4c6e26d98e5e0e703a638ea3
mothibimalebana <mothibi.mm12@gmail.com>
2024-01-20 07:30:01 +0200

    created 0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - Function that fills memory with a constant byte.
+ * @s: Pointer to a to memory area.
+ * @b: constant byte to fill memory with.
+ * @n: number of bytes to fill memory area with.
+ * Return: A pointer to the memory area s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	/**Initialise variables**/
+	unsigned int i;
+
+	/**Iterate through memory area b**/
+	i = 0;
+	while (i < n)
+	{
+		*s[i] = b;
+		i++;
+		s++;
+	}
+
+	return (s);
+}
